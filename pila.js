50e7087c86f8c9e412356c69ed6c3d11c37ddd59
Noel Zamora <noelio1290@gmail.com>
2023-01-27 18:19:00 -0600

    nueva pila con metodo para agregar carros, aun sin metodo para extraer auto

--- pila.js
+++ pila.js
@@ -1,44 +1,35 @@
-class Stack {
+class Auto {
+    constructor(marca){
+    this.marca = marca
+    this.prev = null
+    };
+    setPrev(auto){
+        this.prev = auto;
+      }
+}
+
+class Pila{
     constructor(){
-        this.stack = {}
-        this.count = 1
-    };
-    agregarCarros(nombre){
-        this.stack[this.count] = nombre
-        this.count++
-    };
-    sacarUltimoCarro(){
-        this.count--
-        let elemento = this.stack[this.count]
-        delete this.stack[this.count]
-        return elemento
+        this.head = null
+        this.tail = null
     };
 
-    contenido(){
-        console.log(this.stack)
+    addCar(auto){
+        if(!this.head){
+            this.head = auto
+            this.tail = this.head
+        } else {
+            const prevCar = this.tail
+            this.tail = auto
+            this.tail.setPrev(prevCar)
+        }
     };
+}
 
-    cantidad(){
-        console.log(this.count)
-    };
-};
-
-class Auto{
-    constructor(nombre){
-        this.nombre = nombre
-    };
-};
-
-const estacionamiento = new Stack()
-estacionamiento.contenido()
-estacionamiento.agregarCarros("Chevy")
-estacionamiento.agregarCarros("Mustang")
-estacionamiento.agregarCarros("Tsuru")
-estacionamiento.agregarCarros("Ferrari")
-estacionamiento.contenido()
-console.log(estacionamiento.sacarUltimoCarro())
-estacionamiento.contenido()
-console.log(estacionamiento.sacarUltimoCarro())
-console.log(estacionamiento.sacarUltimoCarro())
-console.log(estacionamiento.sacarUltimoCarro())
-estacionamiento.contenido()
+const estacionamiento = new Pila()
+console.log(estacionamiento)
+estacionamiento.addCar(new Auto({marca:"Chevy"}))
+estacionamiento.addCar(new Auto({marca:"Bocho"}))
+estacionamiento.addCar(new Auto({marca:"Cheyenne"}))
+console.log(estacionamiento)
+console.log(estacionamiento.head)
